2ed03d47119039794b1d3c64330796b3ff81e4cd
coolgary100 <bowengary@hotmail.com>
2018-03-07 09:48:58 -0800

    New tuned PID values to the microcontroller. Updated transfer function and PID values in the matlab model. Added in the velocity and corresponding time values in velocity.txt and timeSec.txt

---
+++ POC/Microcontroller_POC_Motor/Serial_To_File/GaryTrimmer.ps1
@@ -0,0 +1,17 @@
+﻿$tempData = Get-Content "temp.txt”
+$timeData = Get-Content "temp.txt”
+$velocityData = Get-Content "temp.txt"
+
+$index = 0
+for($i = 0; $i -lt $tempData.Length; $i++)
+{
+    
+    $timeData[$index] = $tempData[$i];
+    $velocityData[$index] = $tempData[$i+1];
+    $index++;
+    $i++;
+};
+
+$velocityData |  ? {$_.trim() -ne "" }  | Set-Content "velocity.txt"
+$timeData |  ? {$_.trim() -ne "" }  | Set-Content "timesec.txt"
+$tempData = Get-Content "temp.txt"
